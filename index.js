7ed593e8f43865213ff0a0110eaab0b951d0dd1d
Sanja Höbenreich <sanja.hoebenreich@outlook.at>
2020-05-25 10:03:45 +0200

    basic exercises uploaded

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('Hi, my name is Martin!');
+let cars = [ 'Tesla', 'Audi', 'Renault', 'Volvo', 'Mazda', 'Fiat', 'Ferrari' ];
+console.log(cars.sort());
+
+let fruit = [ 'apple', 'banana', 'kiwi' ];
+fruit.push('orange');
+console.log(fruit);
+fruit.pop();
+console.log(fruit);
+
+let animals = [ 'monkey', 'horse', 'dog' ];
+animals.sort();
+console.log(animals);
+animals.unshift('cat');
+console.log(animals);
